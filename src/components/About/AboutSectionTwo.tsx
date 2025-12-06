111b1c900520b4bc73e2ee52be0fa65d09be6d6e
Csoki <csoki@Csoki-MacBook-Pro.local>
2025-12-06 18:20:43 +0100

    kep1.png -> kep1.webp, kep2.png -> kep2.webp cserék az About szekcióban

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -11,7 +11,7 @@ const AboutSectionTwo = () => {
               data-wow-delay=".15s"
             >
               <Image
-                src="/kepek/kep1.png"
+                src="/kepek/kep1.webp"
                 alt="about image"
                 fill
                 className="drop-shadow-three dark:hidden dark:drop-shadow-none"
